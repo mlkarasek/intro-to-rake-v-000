f20930d1dd86007c5d772004e5c934745074bc8b
Monica  Karasek <mlkarasek@gmail.com>
2018-08-07 02:28:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,4 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
-    Student.create_table
-  end
-end
-
 end
